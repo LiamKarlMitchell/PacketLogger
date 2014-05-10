6a1a54b91654cd524574110a6a3a3e49f986c5a6
Chris Clower <clowerweb@fullsail.edu>
2014-05-09 21:11:51 -0400

    angular-bootstrap modal test

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,41 @@
-angular.module('myModule', ['ui.bootstrap']);
+angular.module('packetLogger', ['ui.bootstrap']);
+
+var ModalCtrl = function($scope, $modal, $log) {
+  $scope.items = ['item1', 'item2', 'item3'];
+
+  $scope.open = function(size) {
+    var modalInstance = $modal.open({
+      templateUrl: 'modal-content.html',
+      controller: ModalInstanceCtrl,
+      size: size,
+      resolve: {
+        items: function() {
+          return $scope.items;
+        }
+      }
+    });
+
+    modalInstance.result.then(function(selectedItem) {
+      $scope.selected = selectedItem;
+    }, function() {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+};
+
+var ModalInstanceCtrl = function($scope, $modalInstance, items, $log) {
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function() {
+    $modalInstance.close($scope.selected.item);
+    $log.info('Modal OK selected.');
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+    $log.info('Modal Cancel selected.');
+  };
+};
